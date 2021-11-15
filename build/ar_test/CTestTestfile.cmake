dda397a4c0b6f50943be5b70d5d81cd75b0e14b1
Class Account <ee106a-adl@c105-6.local>
2021-11-14 20:33:27 -0800

    working ros workspace

---
+++ build/ar_test/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/cc/ee106a/fl21/class/ee106a-adl/ros_workspaces/Robo-Jenga/src/ar_test
+# Build directory: /home/cc/ee106a/fl21/class/ee106a-adl/ros_workspaces/Robo-Jenga/build/ar_test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
