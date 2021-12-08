0596abe011ecd307e54fb7e0c0dbb8a806ea3f51
jordanheinemann <jheinemann@berkeley.edu>
2021-12-07 23:10:40 -0800

    refactor with planning functions, style

--- src/jenga_movement/src/jenga_bot.py
+++ src/jenga_movement/src/jenga_bot.py
@@ -16,9 +16,6 @@ BLOCK_LENGTH = .075
 
 MAX_FORCE = 1
 
-# Tag Frame
-TAG_FRAME = "aruco_tag_0"
-
 class Jenga_Bot:
     # Frames:
     # aruco_tag_0, pokey_stick, left_hand, grabby_claw, right_hand
@@ -44,25 +41,54 @@ class Jenga_Bot:
         """ Load cell subscriber callback """
         self.FORCE = force
 
-    def plan_stick_movement(self, g):
+    def plan_stick_movement(self, stick_target_t):
         """Take target stick frame relative to base and return plan to move there"""
-        pass
+        # Publish to tf
+        self.frame_pub.publish(helpers.g_to_tf(stick_target_t, "base", self.STICK_FRAME))
+        # Get Homogenous Matrix g from tf info
+        stick_to_hand_t = helpers.tf_to_g(self.tfBuffer.lookup_transform(self.STICK_FRAME, self.LEFT_HAND_FRAME, rospy.Time(0)))
+        # Get desired end-effector configuration matrix
+        hand_target_t = np.matmul(stick_target_t, stick_to_hand_t)
+        #CREATE HAND TARGET POSE     
+        hand_target_pose = helpers.g_to_pose(hand_target_t, "base")
+        # PUBLISH TO TF
+        self.frame_pub.publish(helpers.g_to_tf(hand_target_t, "base", "left_hand_target"))
+        # GENERATE AND EXECUTE PLAN
+        plan = self.stick_planner.plan_to_pose(hand_target_pose, [])
+        return plan
 
-    def plan_claw_movement(self, g):
+    def plan_claw_movement(self, claw_target_t):
         """Take target claw frame relative to base and return plan to move there"""
-        pass
+        # Publish to tf
+        self.frame_pub.publish(helpers.g_to_tf(claw_target_t, "base", self.CLAW_FRAME))
+        # Get Homogenous Matrix g from tf info
+        claw_to_hand_t = helpers.tf_to_g(self.tfBuffer.lookup_transform(self.CLAW_FRAME, self.RIGHT_HAND_FRAME, rospy.Time(0)))
+        # Get desired end-effector configuration matrix
+        hand_target_t = np.matmul(claw_target_t, claw_to_hand_t)
+        #CREATE HAND TARGET POSE     
+        hand_target_pose = helpers.g_to_pose(hand_target_t, "base")
+        # PUBLISH TO TF
+        self.frame_pub.publish(helpers.g_to_tf(hand_target_t, "base", "right_hand_target"))
+        # GENERATE AND EXECUTE PLAN
+        plan = self.claw_planner.plan_to_pose(hand_target_pose, [])
+        return plan
 
     ### PUBLIC ###
 
     def __init__(self):
         """ Setup Jenga Bot object. Init subscriber, service proxy"""
+        
         # Push Stick Frame
-        self.PUSH_STICK_FRAME = "pokey_stick"
-        # Gripper Frame
-        self.GRIPPER_FRAME = "grabby_claw"
+        self.STICK_FRAME = "pokey_stick"
+        # Claw Frame
+        self.CLAW_FRAME = "grabby_claw"
         # Left Hand Frame
         self.LEFT_HAND_FRAME = 'left hand'
         # End-Effector Planners
+        self.RIGHT_HAND_FRAME = 'right_hand'
+        # Tag frame
+        self.TAG_FRAME = "aruco_tag_0"
+
         self.stick_planner = PathPlanner("left_arm")
         # Right Hand
         self.claw_planner = PathPlanner("right_arm")
@@ -88,6 +114,7 @@ class Jenga_Bot:
 
     def open_claw(self):
         """ Call service proxy to open claw """
+
         rospy.wait_for_service('open_gripper')
         try:
             send_command = rospy.ServiceProxy('open_gripper', GripperSrv)
@@ -98,6 +125,7 @@ class Jenga_Bot:
 
     def close_claw(self):
         """ Call service proxy to close claw """
+        
         rospy.wait_for_service('close_gripper')
         try:
             send_command = rospy.ServiceProxy('close_gripper', GripperSrv)
@@ -117,54 +145,37 @@ class Jenga_Bot:
         tag_t = helpers.tf_to_g(self.tfBuffer.lookup_transform("base", self.TAG_FRAME, rospy.Time(0)))
         #GET STICK TARGET IN SPATIAL FRAME (FROM CURRENT AR TAG POSITION)
         stick_target_t = np.matmul(tag_t, tag_to_stick_target_t)
-        # Publish to tf
-        self.frame_pub.publish(helpers.g_to_tf(stick_target_t, "base", self.PUSH_STICK_FRAME))
-        # Get Homogenous Matrix g from tf info
-        stick_to_hand_t = helpers.tf_to_g(self.tfBuffer.lookup_transform(self.PUSH_STICK_FRAME, self.LEFT_HAND_FRAME, rospy.Time(0)))
-        # Get desired end-effector configuration matrix
-        hand_target_t = np.matmul(stick_target_t, stick_to_hand_t)
-        #CREATE HAND TARGET POSE     
-        hand_target_pose = helpers.g_to_pose(hand_target_t, "base")
-        # PUBLISH TO TF
-        self.frame_pub.publish(helpers.g_to_tf(hand_target_t, "base", "hand_target"))
-        # GENERATE AND EXECUTE PLAN
-        plan = self.stick_planner.plan_to_pose(hand_target_pose, [])
+        
+        plan = self.plan_stick_movement(stick_target_t)
+
         return plan
 
     def plan_move_stick_down_rows(self, num_rows):
         """ Return plan to move stick down num_rows rows """
 
         # Get trans matrix from base to left hand and convert to homogenous mattrix
-        hand_t = helpers.tf_to_g(self.tfBuffer.lookup_transform("base", self.LEFT_HAND_FRAME, rospy.Time(0)))
+        stick_t = helpers.tf_to_g(self.tfBuffer.lookup_transform("base", self.STICK_FRAME, rospy.Time(0)))
         # Move down the Jenga tower by 0.015 mm down the y-axis
         move_down_trans = np.array([0, 0, -1 * num_rows * BLOCK_HEIGHT])
         move_down_t = transformations.translation_matrix(move_down_trans)
 
-        hand_target_t = np.matmul(move_down_t, hand_t) #move down in spatial frame
+        stick_target_t = np.matmul(move_down_t, stick_t) #move down in spatial frame
 
-        self.frame_pub.publish(helpers.g_to_tf(hand_target_t, "base", "hand_target"))
-
-        hand_target_pose = helpers.g_to_pose(hand_target_t, "base")
-
-        plan = self.stick_planner.plan_to_pose(hand_target_pose, [])
+        plan = self.plan_stick_movement(stick_target_t)
 
         return plan
 
     def plan_push(self):
         """ Plan full push motion to verify in RVIZ"""
 
-        hand_t = helpers.tf_to_g(self.tfBuffer.lookup_transform("base", self.LEFT_HAND_FRAME, rospy.Time(0)))
+        stick_t = helpers.tf_to_g(self.tfBuffer.lookup_transform("base", self.STICK_FRAME, rospy.Time(0)))
 
-        move_forward_trans = np.array([0, 0, 0.375])
+        move_forward_trans = np.array([0, 0, .5 * BLOCK_LENGTH])
         move_forward_t = transformations.translation_matrix(move_forward_trans)
 
-        hand_target_t = np.matmul(hand_t, move_forward_t) #move forward in hand frame
+        stick_target_t = np.matmul(stick_t, move_forward_t) #move forward in hand frame
         
-        self.frame_pub.publish(helpers.g_to_tf(hand_target_t, "base", "hand_target"))
-
-        hand_target_pose = helpers.g_to_pose(hand_target_t)
-        
-        self.stick_planner.plan_to_pose(hand_target_pose, [])
+        self.plan_stick_movement(stick_target_t)
 
     def execute_careful_push(self):
         """ Plan and execute careful push, checking force value along the way. Return bool for success"""
